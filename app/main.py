6e109a6f300d42afe768eef094d37262da458a60
LennartEdbb <lennartedler4@gmail.com>
2026-02-25 08:33:30 +0100

    add Cors for api communication

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.db import engine
 from app.models import Base
 
@@ -7,6 +9,14 @@ from app.Routers.activities import router as activities_router
 from app.Routers.user_actions import router as user_actions_router
 
 app = FastAPI(title="CityTrip API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 def on_startup():
